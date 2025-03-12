372f49b616933f3fdb271efd8c3279b2fc0f2993
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2025-03-13 08:13:32 +1100

    updating syntax and added camera rosbags

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 
-// TO DO: Make program entry point.
-#include "PicassoArm.h"
-#include "PicassoEyes.h"
-#include "PicassoUI.h"
+#include "picasso_arm.h"
+#include "picasso_eyes.h"
+#include "picasso_ui.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PicassoEyes>());
+
+    auto node = std::make_shared<rclcpp::Node>("picasso_bot");
+    
+    auto arm = std::make_shared<PicassoArm>(node);
+    auto eyes = std::make_shared<PicassoEyes>(node);
+    auto ui = std::make_shared<PicassoUI>(node);
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+
     rclcpp::shutdown();
     return 0;
 }
