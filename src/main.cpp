2b63b6139d897a63ac939d3433063e348f2c11e3
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2025-03-19 12:05:00 +1100

    I think I have fixes the repo

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,14 @@
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<rclcpp::Node>("picasso_bot");
+    auto node = std::make_shared<rclcpp::Node>("picasso_bot");  // Added by AI - Is this best practice?
     
-    auto arm = std::make_shared<PicassoArm>(node);
+    // Do we want to have each of these in their own node?
+    auto arm = std::make_shared<PicassoArm>(node); 
     auto eyes = std::make_shared<PicassoEyes>(node);
     auto ui = std::make_shared<PicassoUI>(node);
 
-    rclcpp::executors::MultiThreadedExecutor executor;
+    rclcpp::executors::MultiThreadedExecutor executor;  // Added by AI - what is this?
     executor.add_node(node);
     executor.spin();
 
