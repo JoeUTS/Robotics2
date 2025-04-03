5f52341fa5209731c1408087c0e02c00dc2aee11
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2025-04-03 20:35:29 +1100

    Visualiser + launch.   - Added gazebo to launch of picasso arm

--- src/picasso_eyes/image_controller.h
+++ src/picasso_eyes/image_controller.h
@@ -54,75 +54,48 @@ public:
 
   /// @brief Updates the stored camera image message.
   /// @param incomingMsg 
-  void updateCameraImage(const realsense2_camera_msgs::msg::RGBD::SharedPtr incomingMsg);
+  sensor_msgs::msg::Image updateCameraImage(const realsense2_camera_msgs::msg::RGBD::SharedPtr incomingMsg);
 
   /// @brief Gets the stored camera image message.
   /// @return realsense2_camera_msgs::msg::RGBD.
   realsense2_camera_msgs::msg::RGBD getStoredImage(void);
-  
-  /// @brief Gets the stored camera depth image message.
-  /// @return cv::Mat.
-  cv::Mat getDepthImage(void);
-
-  /// @brief Gets the stored camera RGB image message.
-  /// @return cv::Mat.
-  cv::Mat getRGBImage(void);
-
-  /// @brief Gets the stored camera RGB image message.
-  /// @return sensor_msgs::msg::CameraInfo.
-  sensor_msgs::msg::CameraInfo getRGBInfo(void);
-
-  /// @brief Gets the stored camera depth image message.
-  /// @return sensor_msgs::msg::CameraInfo.
-  sensor_msgs::msg::CameraInfo getDepthInfo(void);
-
-  /// @brief Gets the stored camera message header.
-  /// @return std_msgs::msg::Header.
-  std_msgs::msg::Header getHeader(void);
 
   /// @brief Blur and image via median blur using the given kernal size.
   /// @param image Image to blur.
   /// @param kernalSize Kernal size.
-  void medianBlurImage(cv::Mat &image, const int kernalSize) {
+  void editImageBlurMedian(cv::Mat &image, const int kernalSize) {
     cv::medianBlur(image, image, kernalSize);
   }
 
   /// @brief Blur and image via gaussian blur using the given kernal size.
   /// @param image Image to blur.
   /// @param kernalSize Kernal size.
-  void gaussianBlurImage(cv::Mat &image, const int kernalSize) {
+  void editImageBlurGaussian(cv::Mat &image, const int kernalSize) {
     cv::GaussianBlur(image, image, cv::Size(kernalSize, kernalSize), 0);
   }
 
   /// @brief Converts an image to greyscale.
   /// @param image image to convert.
-  void greyscaleImage(cv::Mat &image) {
+  void editImageGreyscale(cv::Mat &image) {
     cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
   }
 
   /// @brief Thresholds an image with the given threshold
   /// @param image image to threshold.
   /// @param thresh threshold value.
-  void thresholdImage(cv::Mat &image, const int thresh) {
+  void editImageThreshold(cv::Mat &image, const int thresh) {
     cv::threshold(image, image, thresh, 255, cv::THRESH_BINARY);
-  }
-
-  /// @brief Inverse thresholds an image with the given threshold
-  /// @param image image to threshold.
-  /// @param thresh threshold value.
-  void thresholdInverseImage(cv::Mat &image, const int thresh) {
-    cv::threshold(image, image, thresh, 255, cv::THRESH_BINARY_INV);
   }
 
   /// @brief Thresholds an image via adaptive gausian thresholding.
   /// @param image image to threshold.
-  void adaptiveThresholdImage(cv::Mat &image, const int kernalSize) {
+  void editImageThresholdAdaptive(cv::Mat &image, const int kernalSize) {
     cv::adaptiveThreshold(image, image, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, kernalSize, 2);
   }
 
   /// @brief Thresholds an image via Otsu's method thresholding.
   /// @param image image to threshold.
-  void otsuThresholdImage(cv::Mat &image) {
+  void editImageThresholdOtsu(cv::Mat &image) {
     cv::threshold(image, image, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
   }
 
@@ -135,30 +108,23 @@ public:
   /// @return cv::Scalar.
   cv::Scalar getNextColour(void);
 
-  /// @brief Split an image into RGB channels.
-  /// @param image image to split.
-  /// @return std::array<cv::Mat, 3> RGB colour channels.
-  std::array<cv::Mat, 3> splitImage(cv::Mat &image);
-
   /// @brief Convert image msg to cv::Mat.
   /// @param imageMsg sensor_msgs::msg::Image
   /// @return cv::Mat.
-  cv::Mat msg2Mat(sensor_msgs::msg::Image &imageMsg);
+  cv::Mat msg2Mat(const sensor_msgs::msg::Image &imageMsg);
 
-  /// @brief Detects edges in image using canny edge detection. \n
-  ///        If multichannel input image, will convert to greyscale and blur.
+  /// @brief Detects edges in image using canny edge detection.
   /// @param image Input image.
   /// @param thresh [0-1]. Edge detection wighting from lower (0) favoring fewer edges to upper (1) favoring more.
   void detectEdges(cv::Mat &image, float thresh = 0.33);
 
-  /// @brief Find contours in an image. \n 
-  ///        If multichannel input image, will convert to greyscale, blur and threshold.
-  /// @param image Input image. If multichannel, will turn to greyscale and blur.
+  /// @brief Find contours in an image.
+  /// @param image Input image. Must be single channel image.
+  /// @param scale Value to scale image by.
   /// @return List of contour objects.
-  std::map<int, std::shared_ptr<Contour>> getContours(cv::Mat &image);
+  std::map<int, std::shared_ptr<Contour>> getContours(const cv::Mat &image, const float scale);
 
-  /// @brief Calculate averate intensity of image. \n
-  ///        If multichannel input image, will convert to greyscale and blur.
+  /// @brief Calculate averate intensity of image. 
   /// @param image Input image.
   /// @return [0-255]. Average intensity.
   int calculateAverageIntensity(cv::Mat &image);
@@ -171,7 +137,6 @@ private:
   
   // Threadding stuff.
   bool detectionRunning_ = false; // True when detection loop is active.
-  
 
   // Messages.
   realsense2_camera_msgs::msg::RGBD lastCameraMsg_;  // Holds copy of last received image msg.
