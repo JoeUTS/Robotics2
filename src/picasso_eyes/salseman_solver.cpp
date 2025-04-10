e6e386ccb6ecb066c56ccb9737833585670d69b5
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2025-04-10 10:26:15 +1000

    Updated salesman

--- src/picasso_eyes/salseman_solver.cpp
+++ src/picasso_eyes/salseman_solver.cpp
@@ -1,5 +1,13 @@
 #include "salseman_solver.h"
 
-SalsemanSolver(std::map<int, std::shared_ptr<Contour>> contourList) : contourList_(contourList) {
+SalsemanSolver() {
 
+}
+
+void setContourList(std::map<int, std::shared_ptr<Contour>> contourList) {
+  contourList_ = contourList;
+}
+
+std::vector<std::pair<int, bool>> getTravelOrder(void) {
+  return travelOrder_;
 }
--- src/picasso_eyes/salseman_solver.h
+++ src/picasso_eyes/salseman_solver.h
@@ -12,10 +12,16 @@
 
 class SalsemanSolver : public rclcpp::Node {
 public:
-  SalsemanSolver(std::map<int, std::shared_ptr<Contour>> contourList);
+  SalsemanSolver(void);
+
+  void setContourList(std::map<int, std::shared_ptr<Contour>> contourList);
+
+  std::vector<std::pair<int, bool>> getTravelOrder(void);
 
   private:
-  std::map<int, std::shared_ptr<Contour>> contourList_;
+  std::map<int, std::shared_ptr<Contour>> contourList_; // List of contours.
+
+  std::vector<std::pair<int, bool>> travelOrder_;     // List of travel order.
 };
 
 #endif // SALSEMANSOLVER_H
